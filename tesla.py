8885a9431b704cb8d8b86b7ceb835106c25abfab
Titas <titas.januson@gmail.com>
2021-01-11 12:01:30 +0200

    Add docstrings

--- tesla.py
+++ tesla.py
@@ -1,5 +1,44 @@
 class Tesla:
-    # WRITE YOUR CODE HERE
+    """Class reperesenting a Tesla car in OOP
+
+    ...
+
+    Parameters:
+    ----------
+    model (str): 
+        model of Tesla
+    color (str): 
+        color of the car
+    autopilot (bool): 
+        does the car have autopilot default False
+    efficienct (float): 
+        how efficient is the car daufault 0.3
+
+    Attributes:
+    ----------
+    seats_count (int): 
+        number of seats must be more than 2
+    is_locked (bool): 
+        whether the car is locked
+
+    Methods:
+    -------
+    lock() and unlock():
+        changes is_locked variable
+    open_doors():
+        opens the door
+    check_battery_level():
+        prints current battery level
+    drive(travel_range):
+        reduces charge level by a formula
+    welcome():
+        prints a welcome message
+    autopilot(obsticle):
+        prints if obsticle was avoided
+
+
+    """
+
     def __init__(self, model: str, color: str, autopilot: bool = False,
                  efficiency: float = 0.3):
         self.__model = model
@@ -9,7 +48,7 @@ class Tesla:
         self.__seats_count = 5
         self .__autopilot = autopilot
         self.__efficiency = efficiency
-    
+
     @property
     def color(self) -> str:
         return self.__color
@@ -21,7 +60,7 @@ class Tesla:
     @property
     def seats_count(self) -> str:
         return self.__seats_count
-    
+
     @seats_count.setter
     def seats_count(self, new_count) -> None:
         if new_count < 2:
@@ -46,15 +85,22 @@ class Tesla:
 
     def check_battery_level(self) -> str:
         return f"Battery charge level is {self.__battery_charge}%"
- 
+
     def charge_battery(self):
         self.__battery_charge = 100
         self.check_battery_level()
 
     def drive(self, travel_range: float):
+        """Return charge level after driving
+
+            Paramaters:
+                travel_range (float): distance which should be traveled
+
+            Returns:
+                battery_level (str): current charge level 
+        """
         battery_discharge_percent = travel_range * self.__efficiency
         if self.__battery_charge - battery_discharge_percent >= 0:
-            # ADD YOUR CODE
             self.__battery_charge -= battery_discharge_percent
             return self.check_battery_level()
         else:
